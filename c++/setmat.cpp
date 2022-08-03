8be73979a8eca46a4b95ca832c98e6035100c931
Kundan-ojha <aryagmast111@gmail.com>
2022-08-03 23:37:06 +0530

    new line added

--- c++/setmat.cpp
+++ c++/setmat.cpp
@@ -27,8 +27,22 @@ int main()
         }
     }
 
-    for(int i = 0;i<row.size();i++)
+    for(int i = 0;i<n;i++)
     {
-        if(arr[i][])
+        for(int j=0;j<m;j++)
+        {
+            if(row[i] == 0 || col[j] == 0)
+            {
+                arr[i][j] = 0;
+            }
+        }   
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j = 0;j<m;j++)
+        {
+            cout << arr[i][j] <<" ";
+        }
+        cout << endl;
     }
 }
