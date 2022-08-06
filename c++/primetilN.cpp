ad8bd564582834e86857a70a04d42310f6f57ea1
Kundan-ojha <aryagmast111@gmail.com>
2022-08-06 16:54:32 +0530

    prime til n in c++

---
+++ c++/primetilN.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    for(int i=n;i<=m;i++)
+    {
+        int  f =0;
+        for(int j=2;j*j<n;j++)
+        {
+            if(j%i == 0){
+                f = 1;
+                break;
+            }
+        }
+        if(f == 0)
+        {
+            cout << i<<"\n";
+        }
+    }
+}
