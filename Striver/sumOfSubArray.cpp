ab3b0e04f2c2f52b3b87531dd7f2c71453bcb1f5
Kundan-ojha <aryagmast111@gmail.com>
2022-08-11 13:28:50 +0530

    max sum of subarray with naive approach

---
+++ Striver/sumOfSubArray.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxSubArray(vector<int>&nums,vector<int>&subarray){
+    int max_sum = INT_MIN;
+    int n = nums.size();
+    if(n==1)
+    {
+        return nums[0];
+    }
+    
+    for(int i=0;i<=n-1;i++)
+    {
+        for(int j=i;j<=n-1;j++){
+            int sum =0;
+            for(int k =i;k<=j;k++)
+            {
+                sum = sum+nums[k];
+            }
+            if(sum > max_sum){
+                subarray.clear();
+                max_sum = sum;
+                subarray.push_back(i);
+                subarray.push_back(j);
+            }
+        }
+    }
+    return max_sum;
+}
+int main()
+{
+    vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+    vector<int>subarray;
+    int lon = maxSubArray(arr,subarray);
+    cout << "The longest subarray with max sum is "<<lon <<endl;
+    cout << "The subarray is "<<endl;
+    for(int i= subarray[0];i<= subarray[1];i++)
+    {
+        cout << arr[i]<<" ";
+    }
+}
