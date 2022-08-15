79f8eed91a880e506e4e0af719197f6c42d7f460
Kundan-ojha <aryagmast111@gmail.com>
2022-08-15 19:06:31 +0530

    new folder

---
+++ Striver/SortAnArray012.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int nums[] = {2,0,2,1,1,0};
+    int s = nums.size();
+    int z =0,o=0,t=0;
+    for(int i=0;i<nums.length();i++)
+    {
+        if(nums[i] == 0)
+        {
+            z++;
+        }else if(nums[i] == 1){
+            o++;
+        }
+        else{
+            t++;
+        }
+    }
+    for(int i=0;i<z;i++)
+    {
+        cout << 0<<" ";
+    }
+    for(int i=0;i<o;i++)
+    {
+        cout << 1 <<" ";
+    }
+    for(int i =0;i<t;i++)
+    {
+        cout << 2 << " ";
+    }
+
+}
