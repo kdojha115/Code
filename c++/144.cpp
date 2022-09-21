ffe4ab829b904435af8334377bc4ddc904e7aaa8
Kundan-ojha <aryagmast111@gmail.com>
2022-09-21 16:26:08 +0530

    comments add for hint

--- c++/144.cpp
+++ c++/144.cpp
@@ -1,8 +1,11 @@
+// Binary Tree Iterative Preorder Traversal 
+
 #include <iostream>
 #include <vector>
 #include <stack>
 using namespace std;
 
+//Create node
 struct TreeNode
 {
     int val;
@@ -20,35 +23,40 @@ struct TreeNode *newNode(int data)
 
 vector<int> preorderTraversal(TreeNode *root)
 {
-
+    //create vector
     vector<int> preorder;
+    //set base 
     if (root == NULL)
         return preorder;
-
+    // create a stack and push root
     stack<TreeNode *> st;
     st.push(root);
 
     while (!st.empty())
     {
+        // take top value of stack as a root and than pop from stack
         root = st.top();
         st.pop();
-
+        // push val of root in vector
         preorder.push_back(root->val);
-
+        // check right value of root if not null than push in stack
         if (root->right != NULL)
         {
             st.push(root->right);
         }
+        // // check left value of root if not null than push in stack
         if (root->left != NULL)
         {
             st.push(root->left);
         }
     }
+    // after complete loop return vector
     return preorder;
 }
 
 int main()
 {
+    // create a tree and set value
     struct TreeNode * root = newNode(1);
     root->left = newNode(2);
     root->right = newNode(7);
@@ -56,13 +64,16 @@ int main()
     root->left->right = newNode(4);
     root->left->right->left = newNode(5);
     root->left->right->right = newNode(6);
-
+    
+    //create vector and set value by call a function by passing root as a parameter  
     vector<int> preorder;
     preorder = preorderTraversal(root);
 
+    // print vector
     for (int i = 0; i < preorder.size(); i++)
     {
         cout << preorder[i] << " ";
     }
     cout << endl;
+    return 0;
 }
