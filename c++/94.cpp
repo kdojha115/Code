e1ad2fc94f48f8d985c8b0d931daac0c76370f91
Kundan-ojha <aryagmast111@gmail.com>
2022-09-21 17:53:53 +0530

    comments added for hint

--- c++/94.cpp
+++ c++/94.cpp
@@ -6,6 +6,7 @@
 
 using namespace std;
 
+// struct node
 struct TreeNode
 {
     int val;
@@ -22,33 +23,48 @@ struct TreeNode *newNode(int data)
     return (node);
 }
 
+// funtion
 vector<int> inorderTraversal(TreeNode *root)
 {
+    // create stack
     stack<TreeNode *> st;
+    
+    // create a node and put root as a value
     TreeNode *node = root;
+
+    // create vector
     vector<int> inorder;
     while (true)
     {
         if (node != NULL)
         {
+            // push node in stack and set value left of node as a node
             st.push(node);
             node = node->left;
         }
         else
         {
+            // check stack is empty or not
             if (st.empty() == true)
                 break;
+
+            // set top value of stack in node than pop from stack
             node = st.top();
             st.pop();
+
+            // push value of node in vector than set right of node as a node
             inorder.push_back(node->val);
             node = node->right;
         }
     }
+
+    // return vector
     return inorder;
 }
 
 int main()
 {
+    // create a tree and set value
     struct TreeNode *root = newNode(1);
     root->left = newNode(2);
     root->right = newNode(3);
@@ -57,12 +73,15 @@ int main()
     root->left->right->left = newNode(6);
     root->left->right->right = newNode(7);
 
+    // create a vector and store return value by calling function
     vector<int> inorder;
     inorder = inorderTraversal(root);
 
+    // print vector
     for (int i = 0; i < inorder.size(); i++)
     {
         cout << inorder[i] << " ";
     }
     cout << endl;
+    return 0;
 }
