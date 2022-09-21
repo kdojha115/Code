9125ed40d2b7786ef45608d2e3a57aee75615b50
Kundan-ojha <aryagmast111@gmail.com>
2022-09-21 15:49:43 +0530

    code update

--- c++/102.cpp
+++ c++/102.cpp
@@ -1,30 +1,73 @@
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root == NULL) return ans;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            vector<int>level;
-            for(int i=0;i<size;i++)
-            {
-                TreeNode *node = q.front();
-                q.pop();
-                if(node->left != NULL)
-                {
-                    q.push(node->left);
-                }
-                if(node->right != NULL){
-                    q.push(node->right);
-                }
-                level.push_back(node->val);
-            }
-            ans.push_back(level);
-            
-        }
-        return ans;
-        
-    }
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left, *right;
 };
+
+struct TreeNode *newNode(int data)
+{
+    struct TreeNode *node = new struct TreeNode;
+    node->val = data;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+
+
+vector<vector<int> > levelOrder(TreeNode *root)
+{
+    vector<vector<int> > ans;
+    if (root == NULL)
+        return ans;
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int size = q.size();
+        vector<int> level;
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode *node = q.front();
+            q.pop();
+            if (node->left != NULL)
+            {
+                q.push(node->left);
+            }
+            if (node->right != NULL)
+            {
+                q.push(node->right);
+            }
+            level.push_back(node->val);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+
+int main()
+{
+    struct TreeNode * root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(7);
+    root->left->left = newNode(3);
+    root->left->right = newNode(4);
+    root->left->right->left = newNode(5);
+    root->left->right->right = newNode(6);
+
+    vector<vector<int> > levelorder;
+    levelorder = levelOrder(root);
+
+    for (int i = 0; i < levelorder.size(); i++)
+    {
+        for(int j=0;j<levelorder[i].size();j++)
+        {
+            cout << levelorder[i][j] << " ";
+        }
+        cout <<endl;
+    }
+}
