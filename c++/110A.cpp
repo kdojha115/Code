ef0c393ce72bb88185eea9ddba1322f06e57aa49
Kundan-ojha <aryagmast111@gmail.com>
2022-09-23 00:24:00 +0530

    update in code

--- c++/110A.cpp
+++ c++/110A.cpp
@@ -1,4 +1,7 @@
-#include<iostream>
+// Codeforces
+// 110A. Nearly Lucky Number
+
+#include <iostream>
 
 using namespace std;
 
@@ -7,16 +10,21 @@ int main()
     long long int n;
     cin >> n;
     long long int m = 0;
-    while(n>0){
-        if(n%10 == 4 || n%10 == 7){
+    while (n > 0)
+    {
+        if (n % 10 == 4 || n % 10 == 7)
+        {
             m++;
         }
-        n/=10;
+        n /= 10;
     }
-    if(m == 4 || m == 7){
-        cout << "YES"<<endl;
-    }else{
-        cout << "NO" <<endl;
+    if (m == 4 || m == 7)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
     }
     return 0;
 }
