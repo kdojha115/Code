a3dc0535d8256fab200d50720e3ece8a05c516ac
Kundan-ojha <aryagmast111@gmail.com>
2022-09-23 01:19:44 +0530

    codeforces 271 A

--- c++/271A.cpp
+++ c++/271A.cpp
@@ -1,12 +1,50 @@
-#include<iostream>
+// Codeforces
+// 271 A. Beautiful Year
+
+#include <iostream>
 
 using namespace std;
 
-int mian()
+int beautifulYear(int n)
 {
-    string s;
-    cin >> s;
-    while(true){
-        if(s[i] !=)
+    int arr[10];
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = 0;
     }
+    int temp = n;
+    while (temp > 0)
+    {
+        arr[temp % 10] = 1;
+        temp /= 10;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr[i] == 1)
+        {
+            count++;
+        }
+    }
+
+    if (count == 4)
+    {
+        return n;
+    }
+    else
+    {
+        n = n + 1;
+        return beautifulYear(n);
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = beautifulYear(n + 1);
+    cout << ans << endl;
+    return 0;
 }
