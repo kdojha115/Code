ca0eca9a900e46a6eea5bee2a00e56f824c40489
Kundan-ojha <aryagmast111@gmail.com>
2022-09-26 17:57:38 +0530

    leetcode question 215

---
+++ c++/215.cpp
@@ -0,0 +1,41 @@
+// Leetcode
+// 215. Kth Largest Element in an Array
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int findKthLargest(vector<int> &nums, int k)
+{
+    stack<int> st;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++)
+    {
+        st.push(nums[i]);
+    }
+    for (int i = 0; i < k - 1; i++)
+    {
+        st.pop();
+    }
+    return st.top();
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    int k;
+    cin >> k;
+    int a = findKthLargest(v, k);
+    cout << a << endl;
+    return 0;
+}
