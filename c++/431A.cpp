7239bda0b8eddef8ab56ac6d1d4585923facfbbe
Kundan-ojha <aryagmast111@gmail.com>
2022-09-28 23:32:24 +0530

    codeforces black square solution

---
+++ c++/431A.cpp
@@ -0,0 +1,37 @@
+// Codeforces
+// 431 A. Black Square
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a1, a2, a3, a4;
+    cin >> a1 >> a2 >> a3 >> a4;
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '1')
+        {
+            ans += a1;
+        }
+        else if (s[i] == '2')
+        {
+            ans += a2;
+        }
+        else if (s[i] == '3')
+        {
+            ans += a3;
+        }
+        else
+        {
+            ans += a4;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
