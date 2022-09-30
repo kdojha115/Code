2cdcc970a2788da7c3c3fd6d80d75bb01bbd434e
Kundan-ojha <aryagmast111@gmail.com>
2022-10-01 00:58:35 +0530

    codeforces 492A solution

--- c++/492A.cpp
+++ c++/492A.cpp
@@ -1,21 +1,28 @@
-#include<iostream>
+// Codeforces
+// 492 A. Vanya and Cubes
+
+#include <iostream>
 
 using namespace std;
+
+int sumOfK(int n)
+{
+    if (n == 1)
+        return 1;
+    return n + sumOfK(n - 1);
+}
 
 int main()
 {
     int n;
     cin >> n;
-    int a=0,k=1;
+    int a = 1, k = 1;
 
-    while(n>k){
-        k=a;
-        while(n>k){
-            n -= k;
-            k--;
-        }
+    while (n >= k)
+    {
         a++;
-        
+        k += sumOfK(a);
     }
-    cout << a-1 <<endl;
+    cout << a - 1 << endl;
+    return 0;
 }
