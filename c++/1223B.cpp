0c7685b48d6e95225c80a0c80f82409156dad5dd
Kundan-ojha <aryagmast111@gmail.com>
2022-10-01 22:08:14 +0530

    CODEFORCES 1223B solution

---
+++ c++/1223B.cpp
@@ -0,0 +1,42 @@
+// Codeforces
+// 1223 B. Strings Equalization
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        string s, t;
+        cin >> s;
+        cin >> t;
+
+        int temp = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            for (int j = 0; j < t.length(); j++)
+            {
+                if (s[i] == t[j])
+                {
+                    temp++;
+                    break;
+                }
+            }
+        }
+        if (temp > 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
