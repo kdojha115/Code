66e6a8d5ea5e5273c0ddabe6007a4f6487d1fffc
kdojha115 <53343743+kdojha115@users.noreply.github.com>
2022-10-13 23:21:34 +0530

    codeforces

---
+++ c++/1742B.cpp
@@ -0,0 +1,40 @@
+// Codeforces
+// 1742 B. Increasing
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin >> arr[i];
+        }
+        sort(arr,arr+n);
+        int flag = 0;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i] == arr[i-1])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0)
+        {
+            cout << "YES" <<endl;
+        }
+        else{
+            cout << "NO" <<endl;
+        }
+    }
+    return 0;
+}
