1d2286ca613dd2b4bda96d4a6157924d586a623c
kdojha115 <53343743+kdojha115@users.noreply.github.com>
2022-10-31 01:49:53 +0530

    algorithm day 11 solution

---
+++ Leetcode/Algorithm1/combinations.cpp
@@ -0,0 +1,25 @@
+// Leetcode
+// Algorithm 1 
+// Day 11
+// 77. Combinations
+
+// Approach 1:
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+		int i = 0;
+		vector<int> p(k, 0);
+		while (i >= 0) {
+			p[i]++;
+			if (p[i] > n) --i;
+			else if (i == k - 1) result.push_back(p);
+			else {
+			    ++i;
+			    p[i] = p[i - 1];
+			}
+		}
+		return result;
+    }
+};
